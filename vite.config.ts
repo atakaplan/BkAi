3ccb499239be836a3739ebce6aa58afdc61fb75a
Atakan Kaplan <atakan.kaplan@bktomorrow.com>
2026-05-15 11:42:27 +0300

    fix: route düzenlemesi.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: '/BkAi/',
+   base: command === 'build' ? '/BkAi/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+}))
